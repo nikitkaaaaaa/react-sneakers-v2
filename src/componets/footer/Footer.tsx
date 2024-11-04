fb1e13ad83886d95fb1eb76b35eb9e05ffe71b7b
nikitka <nikitausufi00@gmail.com>
2024-11-04 12:47:52 +0200

    сделал адаптив footer

--- src/componets/footer/Footer.tsx
+++ src/componets/footer/Footer.tsx
@@ -42,59 +42,87 @@ const Footer = ({ isLoading }: FooterProps) => {
         <>
           <hr className="my-16" />
           <div className="container">
-            <div className="flex justify-between">
-              <div>
-                {mainMenuItems.map((item, index) => (
-                  <div key={index}>
-                    <div
-                      className={`  ${
-                        index === 0 ? "mb-5 text-black" : "mb-4 "
-                      }`}
-                    >
-                      {item}
+            <div className="border hidden md:block">
+              <div className="flex justify-between">
+                <div>
+                  {mainMenuItems.map((item, index) => (
+                    <div key={index}>
+                      <div
+                        className={`  ${
+                          index === 0 ? "mb-5 text-black" : "mb-3 "
+                        }`}
+                      >
+                        {item}
+                      </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
+
+                <div>
+                  {companyInfoItems.map((item, index) => (
+                    <div key={index}>
+                      <div
+                        className={`  ${
+                          index === 0
+                            ? "mb-5 text-black"
+                            : "mb-3 text-gray-500 "
+                        }`}
+                      >
+                        {item}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+
+                <div>
+                  {policyInfoItems.map((item, index) => (
+                    <div key={index}>
+                      <div
+                        className={`  ${
+                          index === 0 ? "mb-5 text-black" : "mb-3 text-gray-500"
+                        }`}
+                      >
+                        {item}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+
+                <div className=" hidden lg:block">
+                  <div className="mb-2.5">Контакты</div>
+                  {contact.map((item, index) => (
+                    <div key={index} className="flex mb-3">
+                      <img src={item.image} alt={item.value} />
+                      <div className="ml-5 text-gray-500">{item.value}</div>
+                    </div>
+                  ))}
+                </div>
               </div>
 
-              <div>
-                {companyInfoItems.map((item, index) => (
-                  <div key={index}>
-                    <div
-                      className={`  ${
-                        index === 0 ? "mb-5 text-black" : "mb-4 text-gray-500"
-                      }`}
-                    >
-                      {item}
+              <div className=" justify-center flex lg:hidden">
+                <div>
+                  {contact.map((item, index) => (
+                    <div key={index} className="flex mb-3">
+                      <img src={item.image} alt={item.value} />
+                      <div className="ml-5 text-gray-500">{item.value}</div>
                     </div>
-                  </div>
-                ))}
-              </div>
-
-              <div>
-                {policyInfoItems.map((item, index) => (
-                  <div key={index}>
-                    <div
-                      className={`  ${
-                        index === 0 ? "mb-5 text-black" : "mb-4 text-gray-500"
-                      }`}
-                    >
-                      {item}
-                    </div>
-                  </div>
-                ))}
-              </div>
-
-              <div>
-                <div className="mb-2.5">Контакты</div>
-                {contact.map((item, index) => (
-                  <div key={index} className="flex mb-4">
-                    <img src={item.image} alt={item.value} />
-                    <div className="ml-5 text-gray-500">{item.value}</div>
-                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
             </div>
+
+            {/* Футер при маленьком экране */}
+            <div className=" font-bold block md:hidden">
+              <div className="border-t border-l border-r py-3.5 pl-3">
+                {mainMenuItems[0]}
+              </div>
+              <div className="border py-3.5 pl-3">{companyInfoItems[0]}</div>
+              <div className="border-b border-l border-r py-3.5 pl-3">
+                {policyInfoItems[0]}
+              </div>
+            </div>
+            {/* Футер при маленьком экране */}
+
             <div className=" flex justify-center items-center  my-16">
               {socials.map((item, index) => (
                 <Link
@@ -106,11 +134,11 @@ const Footer = ({ isLoading }: FooterProps) => {
                 </Link>
               ))}
             </div>
-            <div className="text-gray-500 mb-20">
+            <div className="text-gray-500 mb-20 text-center md:text-left">
               2024 © WAYOFF — это розничный магазин, специализирующийся на
               продаже лимитированных вещей. В нашем широком ассортименте
               присутствуют исключительно новые и оригинальные товары.
-              <div>Разрабочик Юсуфи Никита</div>
+              <div className="mt-2">Разрабочик Юсуфи Никита</div>
             </div>
           </div>
         </>
